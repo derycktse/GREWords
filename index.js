afdc1c12268ece3ca09db23e9f444742e7724aa7
Derycktse <285205451@qq.com>
2017-06-13 11:25:49 +0800

    add format html

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http')
 const fs = require('fs')
 const utils = require('./lib/utils')
 const path = require('path')
+const formatHTML = require('./lib/format-html')
 
 const PORT = process.env.PORT || 8080
 
@@ -13,21 +14,21 @@ setInterval(function () {
 
 
 const server = http.createServer((req, res) => {
-  let today = utils.getBeijingTimeString(new Date())
+  let today = utils.getBeijingTimeString(new Date(), 'yyyyMMdd')
   let todayPath = path.resolve(__dirname, './results/' + today + '.txt')
   fetchData()
   if (!fs.existsSync(todayPath)) {
-    res.end(`today no data`)
+    res.end(`${today} has no data`)
     return
   }
 
   fs.readFile(todayPath, (err, data) => {
     if (err) throw err
-    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
     res.write(`${today}数据如下：`)
-    data = data.toString().replace(/(?:\r\n|\r|\n)/g, '<br />');
-    res.end(data,  ()=>{
-      console.log('data has been send')
+    let content = formatHTML(data.toString())
+    res.end(content, () => {
+      console.log(`${today} data has been send`)
     })
   })
 

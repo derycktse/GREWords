da9045cabe88038667692596413a7d8dda8f8a6d
Derycktse <285205451@qq.com>
2017-06-15 14:06:51 +0800

    add findDocuments & insertDocuments

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 const dbHandler = require('./lib/db-handler')
-
-
-dbHandler.insertDocuments({ "name": `deryck${Date.now()}` })
+/*
+let arr = [{ "date": "20170615", "name": `deryck${Date.now()}`, "pronun": "ʌp'hi:vl" }, { "date": "20170615", "name": `lindsey${Date.now()}`, "pronun": "'rʌmb(ə)liŋ" }]
+dbHandler.insertDocuments(arr)
   .then((result) => {
     console.log('insert success')
     console.log(result)
   })
   .catch(err => {
+    console.log('level 1 err accur ')
+    console.log(err)
+  })
+*/
 
+
+  dbHandler.findDocuments({date:'20170615'}).then(docs=>{
+    console.log('succss')
+    console.log(docs)
   })

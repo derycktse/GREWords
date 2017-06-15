b8a540806ebbb80cab29365e5a333ec5215a17b0
Derycktse <285205451@qq.com>
2017-06-15 16:28:42 +0800

    use mongodb to store data

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const dbHandler = require('./lib/db-handler')
+// const dbHandler = require('./lib/db-handler')
 /*
 let arr = [{ "date": "20170615", "name": `deryck${Date.now()}`, "pronun": "ʌp'hi:vl" }, { "date": "20170615", "name": `lindsey${Date.now()}`, "pronun": "'rʌmb(ə)liŋ" }]
 dbHandler.insertDocuments(arr)
@@ -13,7 +13,66 @@ dbHandler.insertDocuments(arr)
 */
 
 
-  dbHandler.findDocuments({date:'20170615'}).then(docs=>{
-    console.log('succss')
-    console.log(docs)
-  })
+// dbHandler.findDocuments({date:'20170615'}).then(docs=>{
+//   console.log('succss')
+//   console.log(docs)
+// })
+
+
+let DBHandler = require('./lib/db-handler')
+let config = require('./config')
+let _ = require('lodash')
+
+class GreHandler extends DBHandler {
+  constructor(config) {
+    super(config)
+  }
+  pruneData(wordList = [], filter = {}) {
+    return new Promise((resolve, reject) => {
+      this.findDocuments(filter).then(docs => {
+        if (!docs) {
+          return resolve(wordList)
+        }
+        _.remove(wordList, (n) => {
+          return _.find(docs, (o) => {
+            return o.name === n.name
+          })
+        })
+        return resolve(wordList)
+      })
+    })
+  }
+
+  formatData(arr) {
+    let today = new Date()
+    let todayString = utils.getBeijingTimeString(today, 'yyyyMMdd')
+
+    let wordsList = []
+    let pattern = /(.*)【(.*)】/
+    arr.forEach(val => {
+      let matches = val.match(pattern)
+      if (!matches) return
+      wordsList.push({
+        word: matches[1],
+        phonetic: matches[2],
+        date: todayString
+      })
+    })
+
+    return wordsList
+  }
+}
+
+let greDBHandler = new GreHandler({
+  url: config.db,
+  collectionName: config.wordsCollection
+})
+
+// greDBHandler.pruneData([{ name: 'deryck149750089385' }, { name: 'deryck' }]).then(result => {
+//   console.log('prune')
+//   console.log(result)
+// })
+console.log(
+  greDBHandler.formatData(["latent【'leitnt】", "mortify【'mɔ:tifai】"])
+
+)

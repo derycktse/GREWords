f1a1c9c73bc650eb83ad84801d90dff2023052a9
Derycktse <285205451@qq.com>
2017-06-16 11:08:10 +0800

    use express

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const http = require('http')
-const fs = require('fs')
 const utils = require('./lib/utils')
-const path = require('path')
-const url = require('url')
 const GREHandler = require('./lib/gre-handler')
+const express = require('express')
+let app = express()
+let greRouter = require('./lib/router')
 
 const PORT = process.env.PORT || 8080
 
@@ -15,25 +14,55 @@ setInterval(function () {
 fetchData()
 
 let serverStartTime
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
+//   let today = utils.getBeijingTimeString(new Date(), 'yyyyMMdd')
+
+//   let pUrl = url.parse(req.url)
+//   let pathname = pUrl.pathname
+//   if (pathname !== '/' && !/\/\d{6}/.test(pathname)) {
+//     res.end()
+//     return
+//   }
+//   if (/\/\d{6}/.test(pathname)) {
+//     today = pathname.match(/\/(\d{8})/)[1]
+//   }
+
+
+
+//   let grehl = new GREHandler({
+//     url: process.env.MONGODB_DG || require('./config/').db,
+//     collectionName: process.env.MONGODB_COLLECTION_GRE || require('./config/').wordsCollection
+//   })
+
+//   grehl.findDocuments({
+//     date: today
+//   }).then(result => {
+//     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+//     res.write(`server is started at ${serverStartTime}<br />`)
+//     res.write(`${today}的数据如下：<br />`)
+//     let html = grehl.objList2HTML(result)
+//     res.end(html)
+//   }).catch(err => {
+//     try {
+//       res.end(`server error: ${err.toString()}`)
+//     } catch (ex) {
+//       res.end('error')
+//     }
+//   })
+
+
+// }).listen(PORT, () => {
+//   serverStartTime = new Date()
+//   console.log(`server is listening at ${PORT}`)
+// })
+/*
+app.get('/', (req, res) => {
   let today = utils.getBeijingTimeString(new Date(), 'yyyyMMdd')
-
-  let pUrl = url.parse(req.url)
-  let pathname = pUrl.pathname
-  if (pathname !== '/' && !/\/\d{6}/.test(pathname)) {
-    res.end()
-    return
-  }
-  if (/\/\d{6}/.test(pathname)) {
-    today = pathname.match(/\/(\d{8})/)[1]
-  }
-
-
-
   let grehl = new GREHandler({
     url: process.env.MONGODB_DG || require('./config/').db,
     collectionName: process.env.MONGODB_COLLECTION_GRE || require('./config/').wordsCollection
   })
+
 
   grehl.findDocuments({
     date: today
@@ -52,9 +81,11 @@ const server = http.createServer((req, res) => {
   })
 
 
-}).listen(PORT, () => {
+})
+*/
+
+app.use('/',greRouter)
+app.listen(PORT, () => {
   serverStartTime = new Date()
   console.log(`server is listening at ${PORT}`)
 })
-
-

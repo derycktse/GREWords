0e79e82b7bf3c3967ebe66df0fb592e36a935d29
Derycktse <285205451@qq.com>
2017-06-17 10:34:14 +0800

    add some style

--- index.js
+++ index.js
@@ -13,79 +13,8 @@ setInterval(function () {
 }, 900000)
 fetchData()
 
-let serverStartTime
-// const server = http.createServer((req, res) => {
-//   let today = utils.getBeijingTimeString(new Date(), 'yyyyMMdd')
-
-//   let pUrl = url.parse(req.url)
-//   let pathname = pUrl.pathname
-//   if (pathname !== '/' && !/\/\d{6}/.test(pathname)) {
-//     res.end()
-//     return
-//   }
-//   if (/\/\d{6}/.test(pathname)) {
-//     today = pathname.match(/\/(\d{8})/)[1]
-//   }
-
-
-
-//   let grehl = new GREHandler({
-//     url: process.env.MONGODB_DG || require('./config/').db,
-//     collectionName: process.env.MONGODB_COLLECTION_GRE || require('./config/').wordsCollection
-//   })
-
-//   grehl.findDocuments({
-//     date: today
-//   }).then(result => {
-//     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-//     res.write(`server is started at ${serverStartTime}<br />`)
-//     res.write(`${today}的数据如下：<br />`)
-//     let html = grehl.objList2HTML(result)
-//     res.end(html)
-//   }).catch(err => {
-//     try {
-//       res.end(`server error: ${err.toString()}`)
-//     } catch (ex) {
-//       res.end('error')
-//     }
-//   })
-
-
-// }).listen(PORT, () => {
-//   serverStartTime = new Date()
-//   console.log(`server is listening at ${PORT}`)
-// })
-/*
-app.get('/', (req, res) => {
-  let today = utils.getBeijingTimeString(new Date(), 'yyyyMMdd')
-  let grehl = new GREHandler({
-    url: process.env.MONGODB_DG || require('./config/').db,
-    collectionName: process.env.MONGODB_COLLECTION_GRE || require('./config/').wordsCollection
-  })
-
-
-  grehl.findDocuments({
-    date: today
-  }).then(result => {
-    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-    res.write(`server is started at ${serverStartTime}<br />`)
-    res.write(`${today}的数据如下：<br />`)
-    let html = grehl.objList2HTML(result)
-    res.end(html)
-  }).catch(err => {
-    try {
-      res.end(`server error: ${err.toString()}`)
-    } catch (ex) {
-      res.end('error')
-    }
-  })
-
-
-})
-*/
-
 app.use('/',greRouter)
+app.use('/assets', express.static(require('path').resolve(__dirname, './assets/')))
 app.listen(PORT, () => {
-  serverStartTime = new Date()
   console.log(`server is listening at ${PORT}`)
 })
